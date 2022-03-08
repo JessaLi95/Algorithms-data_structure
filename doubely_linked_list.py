df34430a44ead7dc1f0f169d1f5725a1b04666c7
JessaLi95 <94200640+JessaLi95@users.noreply.github.com>
2022-03-08 20:13:34 +0000

    *

--- doubely_linked_list.py
+++ doubely_linked_list.py
@@ -80,7 +80,44 @@ class DoulelyLinkedList:
                 temp = temp.prev
         return temp
 
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+            
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+        new_node.prev = before
+        before.next = new_node
+        new_node.next = after
+        after.prev = new_node
+        self.length += 1
+        return True
 
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        temp = self.get(index)
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+        temp.prev = None
+        temp.next = None
+        self.length -= 1
+        return temp
 
 
 myDoubleList = DoulelyLinkedList(1)
@@ -89,9 +126,6 @@ myDoubleList.prepend(0)
 myDoubleList.append(3)
 myDoubleList.append(4)
 myDoubleList.append(5)
+myDoubleList.insert(3,9)
+myDoubleList.remove(3)
 myDoubleList.print_list()
-print(myDoubleList.get(0).value)
-print(myDoubleList.get(1).value)
-print(myDoubleList.get(2).value)
-print(myDoubleList.get(3).value)
-print(myDoubleList.get(4).value)
